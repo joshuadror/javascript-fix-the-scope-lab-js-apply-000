85072428a996f1900dd411b4613e8b19585aaba4
Joshua Dror <jdror2@gmail.com>
2017-12-15 17:01:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
-/*global describe, it */
+var animal = 'dog'
 
-describe('Fix the Scope', function() {
-  describe('myAnimal()', function() {
-    it('returns my animal', () => {
-      expect(window.myAnimal()).toEqual('dog')
-    })
-  })
+function myAnimal() {
+  return animal
+}
 
-  describe('yourAnimal()', function() {
-    it('returns your animal', function(){
-      expect(window.yourAnimal()).toEqual('cat')
-    })
+function yourAnimal() {
 
-    it('does not hard-code the answer', function() {
-      expect(window.yourAnimal.toString()).toNotContain("return 'cat'")
-    })
-  })
+  return animal
+}
 
-  describe('add2(n)', function() {
-    it('adds two to n', function() {
-      const n = Math.floor(Math.random() * 1000)
-      expect(window.add2(n)).toEqual(n + 2)
-    })
-  })
+function add2(n) {
+  return n + two
 
-  describe('funkyFunction()', function() {
-    it('returns a function', function() {
-      expect(typeof window.funkyFunction()).toEqual('function')
-    })
-  })
 
-  describe('theFunk', function() {
-    it('is "FUNKY!"', function() {
-      expect(window.theFunk).toEqual('FUNKY!')
-    })
-  })
+  const two = 2
+}
 
-})
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
